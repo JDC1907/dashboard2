f85ad6276b6725e917b82bd6ba06c4560eb61fbf
Jahir Alexander Díaz Cedeño <jaaldiaz@espol.edu.ec>
2024-12-09 05:09:42 -0500

    Update LineChartWeather.tsx

--- src/components/LineChartWeather.tsx
+++ src/components/LineChartWeather.tsx
@@ -1,17 +1,7 @@
 import Paper from '@mui/material/Paper';
-import { LineChart } from '@mui/x-charts/LineChart';
+//import { LineChart } from '@mui/x-charts/LineChart';
 
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const xLabels = [
-    'Page A',
-    'Page B',
-    'Page C',
-    'Page D',
-    'Page E',
-    'Page F',
-    'Page G',
-];
+
 
 export default function LineChartWeather() {
     return (
@@ -24,7 +14,7 @@ export default function LineChartWeather() {
         >
 
             {/* Componente para un gráfico de líneas */}
-            <LineChart
+            {/* <LineChart
                 width={400}
                 height={250}
                 series={[
@@ -32,7 +22,7 @@ export default function LineChartWeather() {
                     { data: uData, label: 'uv' },
                 ]}
                 xAxis={[{ scaleType: 'point', data: xLabels }]}
-            />
+            /> */}
         </Paper>
     );
 }
